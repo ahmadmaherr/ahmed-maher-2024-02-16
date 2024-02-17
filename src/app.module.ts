aaa9abac7059a0abc702d55a3eadb9611d3bd98d
Ahmad Maher <ahmed.maher@178@gmail.com>
2024-02-17 22:58:21 +0200

    created database container

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostsController } from './posts/posts.controller';
+import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './posts/entities/post.entity';
+import { registerAs } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController, PostsController],
+  imports: [PostsModule,
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: parseInt(process.env.DB_PORT, 10),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        entities: [Post],
+        synchronize: true,
+      })],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
